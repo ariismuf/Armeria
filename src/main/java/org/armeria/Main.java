61bd9cfa7c67be4be3bf2cbb60ffa0399838beb6
ariismuf <aris.s338835@cesurformacion.com>
2024-05-04 17:18:54 +0200

    Creación de repositorio

---
+++ src/main/java/org/armeria/Main.java
@@ -0,0 +1,22 @@
+package org.armeria;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("-------------------------");
+        System.out.println("| SELECCIONE UNA OPCIÓN |");
+        System.out.println("-------------------------");
+        System.out.println("| 1.- Añadir arma       |");
+        System.out.println("| 2.- Editar arma       |");
+        System.out.println("| 3.- Eliminar arma     |");
+        System.out.println("| 4.- Inventario actual |");
+        System.out.println("| 5.- Salir             |");
+        System.out.println("-------------------------");
+
+
+
+    }
+}
